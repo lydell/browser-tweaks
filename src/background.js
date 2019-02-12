80404321368a9bde73daf509c996fa0a0ceec66d
Simon Lydell <simon.lydell@gmail.com>
2019-02-12 08:06:36 +0100

    Add some tab keyboard shortcuts

--- src/background.js
+++ src/background.js
@@ -46,6 +46,33 @@ async function start() {
         );
     }
   });
+
+  chrome.commands.onCommand.addListener(command => {
+    switch (command) {
+      case "close-tabs-to-right":
+        chrome.tabs.query({ currentWindow: true }, tabs => {
+          const index = tabs.findIndex(tab => tab.active);
+          const ids =
+            index >= 0
+              ? tabs
+                  .slice(index + 1)
+                  .filter(tab => !tab.hidden && !tab.pinned)
+                  .map(tab => tab.id)
+              : [];
+          chrome.tabs.remove(ids);
+        });
+        break;
+
+      case "toggle-pinned-tab":
+        chrome.tabs.query({ currentWindow: true, active: true }, ([tab]) => {
+          chrome.tabs.update(tab.id, { pinned: !tab.pinned });
+        });
+        break;
+
+      default:
+        console.error("browser-tweaks/background.js: Unknown command", command);
+    }
+  });
 }
 
 start().catch(error => {
