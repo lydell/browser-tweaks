9f867bf81226b6e84d0014f0a08561aebdf06811
Mathieu Ablasou <taupiqueur.kanto@gmail.com>
2024-08-20 18:28:06 +0200

    Use tab context

--- src/background.js
+++ src/background.js
@@ -1,30 +1,22 @@
 async function start() {
-  chrome.commands.onCommand.addListener((command) => {
+  chrome.commands.onCommand.addListener((command, tab) => {
     switch (command) {
       case "close-tabs-to-right":
-        chrome.tabs.query({ currentWindow: true }, (tabs) => {
-          const index = tabs.findIndex((tab) => tab.active);
-          const ids =
-            index >= 0
-              ? tabs
-                  .slice(index + 1)
-                  .filter((tab) => !tab.hidden && !tab.pinned)
-                  .map((tab) => tab.id)
-              : [];
+        chrome.tabs.query({ windowId: tab.windowId }, (tabs) => {
+          const ids = tabs
+            .slice(tab.index + 1)
+            .filter((tab) => !tab.hidden && !tab.pinned)
+            .map((tab) => tab.id);
           chrome.tabs.remove(ids);
         });
         break;
 
       case "duplicate-tab":
-        chrome.tabs.query({ currentWindow: true, active: true }, ([tab]) => {
-          chrome.tabs.duplicate(tab.id);
-        });
+        chrome.tabs.duplicate(tab.id);
         break;
 
       case "toggle-pinned-tab":
-        chrome.tabs.query({ currentWindow: true, active: true }, ([tab]) => {
-          chrome.tabs.update(tab.id, { pinned: !tab.pinned });
-        });
+        chrome.tabs.update(tab.id, { pinned: !tab.pinned });
         break;
 
       default:
